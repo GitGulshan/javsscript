ef3ae4ccbe220db0688e55b6209acfc94644c482
GitGulshan <gulshan.sahuad7@gmail.com>
2025-08-05 00:54:01 +0530

    Operations Covered

--- ConversionOperations/Conversion.js
+++ ConversionOperations/Conversion.js
@@ -1,10 +1,10 @@
-let score = "33abc";
+// let score = "33abc";
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -12,8 +12,8 @@ console.log(valueInNumber);
 
 let IsloggedIn = 1;
 
-let booleanIsLoggedIn = Boolean(IsloggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(IsloggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -21,6 +21,69 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operations *******************
+let value = 4;
+let negValue = -value;
+// console.log(negValue);
+// console.log(typeof negValue);
+// console.log(value);
+
+// // ******************* Arithmetic Operations *******************
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// console.log(2 ** 3);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let ctr1 = "hello";
+let ctr2 = " gulshan";
+
+let newString = ctr1 + ctr2;
+console.log(newString);
+
+let num = 3 + "4";
+let num2 = 3 + 4;
+let num3 = "3" + "4" + 5;
+let num4 = 3 + 4 + "5";
+// console.log(num);
+// console.log(typeof num);
+// console.log(num2);
+// console.log(typeof num2);
+// console.log(num3);
+// console.log(typeof num3);
+// console.log(num4);
+// console.log(typeof num4);
+
+let gameCounter = 100;
+console.log(gameCounter);
+// gameCounter++;
+// console.log(gameCounter);
+// gameCounter--;
+// console.log(gameCounter);
+// gameCounter += 10;
+// console.log(gameCounter);
+// gameCounter -= 10;
+// console.log(gameCounter);
+gameCounter *= 10;
+console.log(gameCounter);
+gameCounter /= 10;
+console.log(gameCounter);
+gameCounter %= 10;
+console.log(gameCounter);
+gameCounter **= 10;
+console.log(gameCounter);
+gameCounter += 10;
+console.log(gameCounter);
+gameCounter -= 10;
+console.log(gameCounter);
