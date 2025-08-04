b005a8df807f2d65ee4ac33b294b476b9c6c52e1
GitGulshan <gulshan.sahuad7@gmail.com>
2025-08-05 00:08:27 +0530

    DataTypes Covered

---
+++ DataTypes/dataTypes.js
@@ -0,0 +1,17 @@
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+// object
+
+console.log(typeof 3); // number
+console.log(typeof "3"); // string
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof Symbol("123"));
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function () {}); // function is an object in javascript
