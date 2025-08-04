107f7ff4a4639f04cf37e44d11af4b6410b53a30
GitGulshan <gulshan.sahuad7@gmail.com>
2025-08-04 19:42:43 +0530

    this is for the const, let and var concepts.

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+const accountId = 456123;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+accountEmail = "xyz@gmail.com";
+accountPassword = "212121";
+accountCity = "Mumbai";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
