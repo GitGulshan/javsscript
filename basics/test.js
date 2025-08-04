e30615240547a72ec9d5bde7c5c8af662f55b37d
GitGulshan <gulshan.sahuad7@gmail.com>
2025-08-04 19:45:24 +0530

    var never use

--- basics/test.js
+++ basics/test.js
@@ -9,4 +9,9 @@ accountEmail = "xyz@gmail.com";
 accountPassword = "212121";
 accountCity = "Mumbai";
 
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 console.table([accountId, accountEmail, accountPassword, accountCity]);
